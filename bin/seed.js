0fa9c99c2b8116cf4ad1fed0d53b266854dd54d5
Alvaro Moral <aruvaro@gmail.com>
2020-04-28 16:40:29 +0200

    empezando mapas

---
+++ bin/seed.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const User = require("../models/user.model");
+
+const dbName = `${process.env.DB}`;
+mongoose.connect(`mongodb://localhost/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const users = [
+  {
+    photoURL: "http://res.cloudinary.com/dw3tnxljg/image/upload/v1587927856/MarvelApp/home3.jpg.jpg",
+    username: "a",
+    email: "a@a.com",
+    password: "$2b$10$KvkDbmJW.7wGvMt1WZl1Je36NR5i9.4lBKF6jWvp31kxQFZla7HHW",
+    status: "Active",
+    confirmationCode: "X6PhZefXL7eXVbfYGFnlV0lwO",
+    location: { type: "Point", coordinates: [40.387071, -3.698199] },
+  },
+];
+
+User.create(users)
+  .then((allUsers) => {
+    console.log("Usuarios creados");
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(`An error ocurred: ${err}`));
