73d022d764504cfa27634b4945d230f406bbcfbd
Alvaro Moral <aruvaro@gmail.com>
2020-04-30 11:41:37 +0200

    search autocomplete working

--- public/javascripts/apiQuery.js
+++ public/javascripts/apiQuery.js
@@ -1,20 +1,47 @@
 const marvelApi = new apiHandler("https://gateway.marvel.com:443/v1/public");
+characterList=[]
+window.addEventListener("load", () => {
 
-document.getElementById("fetch").addEventListener("click", function (event) {
-  event.preventDefault();
-  const charName = document.querySelector(".character-name").value;
-  marvelApi
-    .getCharacter(charName)
-    .then((responseFromApi) => {
-      const charInfo = responseFromApi.data.data.results[0];
-      document.querySelector(".char-img").src = charInfo.thumbnail.path + ".jpg";
-      document.querySelector(".char-name").innerHTML = charInfo.name;
-      charInfo.description
-        ? (document.querySelector(".char-description").innerHTML = charInfo.description)
-        : (document.querySelector(".char-description").innerHTML = "No description available");
+marvelApi.getFileCharacteres()
+        .then(response => {
+            characterList = response.data.split("\n")
+            let tempCharacter = characterList.map(character => ({
+                label: character, value: character
+            }))
+      
+          let input = document.getElementById("character-name");
+          
+            autocomplete({
+                input: input,
+                fetch: function (text, update) {
+                    text = text.toLowerCase();
+                    var suggestions = tempCharacter.filter(n => n.label.toLowerCase().includes(text))
+                    update(suggestions);
+              },
+               
+                onSelect: function (item) {
+                    input.value = item.label;
+                }
+            });
+        }).catch(error => console.log("oOh No! Error is: ", error))
 
-      document.querySelector("#details").href = "/details/" + charInfo.id;
-    })
-    .catch((err) => console.log("No fue posible encontrar el personaje " + err));
+
+  document.getElementById("fetch").addEventListener("click", function (event) {
+    event.preventDefault();
+    const charName = document.querySelector(".character-name").value;
+    marvelApi
+      .getCharacter(charName)
+      .then((responseFromApi) => {
+        const charInfo = responseFromApi.data.data.results[0];
+        document.querySelector(".char-img").src = charInfo.thumbnail.path + ".jpg";
+        document.querySelector(".char-name").innerHTML = charInfo.name;
+        charInfo.description
+          ? (document.querySelector(".char-description").innerHTML = charInfo.description)
+          : (document.querySelector(".char-description").innerHTML = "No description available");
+
+        document.querySelector("#details").href = "/details/" + charInfo.id;
+      })
+      .catch((err) => console.log("No fue posible encontrar el personaje " + err));
+  });
 });
 
